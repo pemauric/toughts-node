c3e90daa332c7d5e3481ee52ab7776c3984ef406
pemauric <pedro.a.mauricio1@gmail.com>
2023-08-07 21:14:05 -0300

    added middleware in /helpers

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -8,4 +8,8 @@ module.exports = class ToughtController {
         res.render('toughts/home')
 
     }
+
+    static async dashboard(req, res) {
+        res.render('toughts/dashboard')
+    }
 }
--- routes/tought.routes.js
+++ routes/tought.routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ToughtsController = require('../controllers/ToughtController');
 
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard', checkAuth, ToughtsController.dashboard);
 router.get('/', ToughtsController.showAll);
 
 module.exports = router
